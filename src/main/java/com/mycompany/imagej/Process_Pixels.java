530cefa27c5563b9d5d1820e5f4e8ae0c0ac1d1f
Logan Walker <logan.walker@me.com>
2022-05-05 01:17:36 -0400

    Reformat as regular plugin

--- src/main/java/com/mycompany/imagej/Process_Pixels.java
+++ src/main/java/com/mycompany/imagej/Process_Pixels.java
@@ -5,185 +5,85 @@
  * See the CC0 1.0 Universal license for details:
  *     http://creativecommons.org/publicdomain/zero/1.0/
  */
-
 package com.mycompany.imagej;
 
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
-import ij.gui.GenericDialog;
-import ij.plugin.filter.PlugInFilter;
+import ij.plugin.PlugIn;
 import ij.process.ImageProcessor;
 
 /**
- * A template for processing each pixel of either
- * GRAY8, GRAY16, GRAY32 or COLOR_RGB images.
+ * A template for processing each pixel of either GRAY8, GRAY16, GRAY32 or
+ * COLOR_RGB images.
  *
  * @author Johannes Schindelin
  */
-public class Process_Pixels implements PlugInFilter {
-	protected ImagePlus image;
+public class Process_Pixels implements PlugIn {
+    protected ImagePlus image;
 
-	// image property members
-	private int width;
-	private int height;
+    public Process_Pixels() {
+        System.out.println("Setting up LiveViewer...");
+        image = IJ.createHyperStack("Test", 2304, 2304, 3, 1, 1, 16);
+        image.show();
+    }
 
-	// plugin parameters
-	public double value;
-	public String name;
+    @Override
+    public void run(String arg) {
+        System.out.println("In run.");
+    }
 
-	@Override
-	public int setup(String arg, ImagePlus imp) {
-		if (arg.equals("about")) {
-			showAbout();
-			return DONE;
-		}
+    /**
+     * Main method for debugging.
+     *
+     * For debugging, it is convenient to have a method that starts ImageJ,
+     * loads an image and calls the plugin, e.g. after setting breakpoints.
+     *
+     * @param args unused
+     */
+    public static void main(String[] args) throws Exception {
+        // set the plugins.dir property to make the plugin appear in the Plugins menu
+        // see: https://stackoverflow.com/a/7060464/1207769
+        Class<?> clazz = Process_Pixels.class;
+        java.net.URL url = clazz.getProtectionDomain().getCodeSource().getLocation();
+        java.io.File file = new java.io.File(url.toURI());
+        System.setProperty("plugins.dir", file.getAbsolutePath());
 
-		image = imp;
-		return DOES_8G | DOES_16 | DOES_32 | DOES_RGB;
-	}
+        // start ImageJ
+        ImageJ IJ_instance = new ImageJ();
 
-	@Override
-	public void run(ImageProcessor ip) {
-		// get width and height
-		width = ip.getWidth();
-		height = ip.getHeight();
+        // open the Clown sample
+        //ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
+        //image.show();
 
-		if (showDialog()) {
-			process(ip);
-			image.updateAndDraw();
-		}
-	}
+        // run the plugin
+        IJ.runPlugIn(clazz.getName(), "");
 
-	private boolean showDialog() {
-		GenericDialog gd = new GenericDialog("Process pixels");
+        /*
+        ImagePlus image = IJ.createHyperStack("Test", 2304, 2304, 3, 1, 1, 16);
+        image.show();
 
-		// default value is 0.00, 2 digits right of the decimal point
-		gd.addNumericField("value", 0.00, 2);
-		gd.addStringField("name", "John");
+        for (int i = 1; i <= 3; i++) {
+            ImageProcessor processor = image.getImageStack().getProcessor(i);
+            short[] pixels = (short[]) processor.getPixels();
+            for (int j = 0; j <= 2304 * 30; j++) {
+                pixels[j] = 1;
+            }
+        }*/
 
-		gd.showDialog();
-		if (gd.wasCanceled())
-			return false;
+        /*
+        System.out.println("Hello World;");
+        
+        URL yahoo = new URL("http://google.com");
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(
+                                yc.getInputStream()));
+        String inputLine;
 
-		// get entered values
-		value = gd.getNextNumber();
-		name = gd.getNextString();
-
-		return true;
-	}
-
-	/**
-	 * Process an image.
-	 * <p>
-	 * Please provide this method even if {@link ij.plugin.filter.PlugInFilter} does require it;
-	 * the method {@link ij.plugin.filter.PlugInFilter#run(ij.process.ImageProcessor)} can only
-	 * handle 2-dimensional data.
-	 * </p>
-	 * <p>
-	 * If your plugin does not change the pixels in-place, make this method return the results and
-	 * change the {@link #setup(java.lang.String, ij.ImagePlus)} method to return also the
-	 * <i>DOES_NOTHING</i> flag.
-	 * </p>
-	 *
-	 * @param image the image (possible multi-dimensional)
-	 */
-	public void process(ImagePlus image) {
-		// slice numbers start with 1 for historical reasons
-		for (int i = 1; i <= image.getStackSize(); i++)
-			process(image.getStack().getProcessor(i));
-	}
-
-	// Select processing method depending on image type
-	public void process(ImageProcessor ip) {
-		int type = image.getType();
-		if (type == ImagePlus.GRAY8)
-			process( (byte[]) ip.getPixels() );
-		else if (type == ImagePlus.GRAY16)
-			process( (short[]) ip.getPixels() );
-		else if (type == ImagePlus.GRAY32)
-			process( (float[]) ip.getPixels() );
-		else if (type == ImagePlus.COLOR_RGB)
-			process( (int[]) ip.getPixels() );
-		else {
-			throw new RuntimeException("not supported");
-		}
-	}
-
-	// processing of GRAY8 images
-	public void process(byte[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (byte)value;
-			}
-		}
-	}
-
-	// processing of GRAY16 images
-	public void process(short[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (short)value;
-			}
-		}
-	}
-
-	// processing of GRAY32 images
-	public void process(float[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (float)value;
-			}
-		}
-	}
-
-	// processing of COLOR_RGB images
-	public void process(int[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (int)value;
-			}
-		}
-	}
-
-	public void showAbout() {
-		IJ.showMessage("ProcessPixels",
-			"a template for processing each pixel of an image"
-		);
-	}
-
-	/**
-	 * Main method for debugging.
-	 *
-	 * For debugging, it is convenient to have a method that starts ImageJ, loads
-	 * an image and calls the plugin, e.g. after setting breakpoints.
-	 *
-	 * @param args unused
-	 */
-	public static void main(String[] args) throws Exception {
-		// set the plugins.dir property to make the plugin appear in the Plugins menu
-		// see: https://stackoverflow.com/a/7060464/1207769
-		Class<?> clazz = Process_Pixels.class;
-		java.net.URL url = clazz.getProtectionDomain().getCodeSource().getLocation();
-		java.io.File file = new java.io.File(url.toURI());
-		System.setProperty("plugins.dir", file.getAbsolutePath());
-
-		// start ImageJ
-		new ImageJ();
-
-		// open the Clown sample
-		ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
-		image.show();
-
-		// run the plugin
-		IJ.runPlugIn(clazz.getName(), "");
-	}
+        while ((inputLine = in.readLine()) != null) 
+            System.out.println(inputLine);
+        in.close();
+         */
+    }
 }
